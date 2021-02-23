c9305fddd3a06a6ef744a3fd9627ba6747474903
David Eadie <david@eadie.net>
2021-02-23 21:09:38 +0000

    Remove 2xl breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     },
     purge: ["./src/**/*.html"],
     theme: {
-      extend: {},
+      container: {
+          screens: {
+              'sm': '640px',
+              'md': '768px',
+              'lg': '1024px',
+              'xl': '1280px',
+          }
+      }
     },
     variants: {},
     plugins: [],
